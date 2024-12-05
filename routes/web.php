6f89f75eed14039c4cb2d6d0f4594cf93bc9d392
Ali Reda <alyredagomaa@gmail.com>
2024-12-05 12:56:31 +0200

    add test end point

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+Route::get('/test', function () {
+    return "APP is live.";
 });
 
 require __DIR__.'/auth.php';
