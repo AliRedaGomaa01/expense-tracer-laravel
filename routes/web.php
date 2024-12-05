177e4c017f51b05dbfeb9b723718d28e43093c2a
Ali Reda <alyredagomaa@gmail.com>
2024-12-05 16:54:58 +0200

    test email

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
-    return "APP is live.";
-});
+    // return "App is live.";
+    Illuminate\Support\Facades\Mail::raw('This is a test email!', function ($message) {
+        $message->to('alyredagomaa@gmail.com')->subject('Test Email');
+    });
 
-require __DIR__.'/auth.php';
+    return response('Test email sent successfully!');
+});
