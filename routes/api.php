80ca4ebc6be7ceeeb2a9facc84bee34123122c64
Ali Reda <alyredagomaa@gmail.com>
2024-12-07 20:22:48 +0200

    update inverified middleware response && make profile paths unverified

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,13 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => ['auth:sanctum' , 'verified']], function () {
+Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::match(['put', 'patch'], '/profile-password', [PasswordController::class, 'update'])->name('profile.password.update');
     Route::match(['put', 'patch'], '/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Route::group(['middleware' => ['auth:sanctum' , 'verified']], function () {
+// });
 
 require __DIR__.'/auth.php';
