c98d97b728595b701313972c652b1801d343631c
Ali Reda <alyredagomaa@gmail.com>
2024-12-09 19:44:12 +0200

    update password controller validation

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
 
 class PasswordController extends Controller
@@ -15,16 +16,28 @@ class PasswordController extends Controller
      */
     public function update(Request $request): JsonResponse
     {
-        $validated = $request->validate([
-            'current_password' => ['required', 'current_password'],
-            'password' => ['required', Password::defaults(), 'confirmed'],
-        ]);
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'current_password' => ['required', 'current_password'],
+                'password' => ['required', Password::defaults(), 'confirmed'],
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $validated = $validator->validated();
 
         $request->user()->update([
             'password' => Hash::make($validated['password']),
         ]);
 
-        return  response()->json([
+        return response()->json([
             'status' => 'success',
             'message' => 'password updated successfully'
         ]);
