11f77a096695b794a939b620d5a9c4c8d2a9f1fd
Ali Reda <alyredagomaa@gmail.com>
2024-12-09 20:31:33 +0200

    reset test path

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
-    # Seed Test User Using Path
-    \App\Models\User::create([
-        'name' => 'Test',
-        'email' => 'test@aly-h.com',
-        'verified_at' => now(),
-        'password' => bcrypt('Test123$$'),
-    ]);
-
-    return "User created successfully.";
-    // return "App is live.";
+    return "App is live.";
 });
