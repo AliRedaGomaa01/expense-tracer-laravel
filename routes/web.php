8ac8b9f639d267d4ae75248aa8167c6b92b312a8
Ali Reda <alyredagomaa@gmail.com>
2024-12-09 20:30:19 +0200

    Seed Test User Using Path

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
-use App\Mail\VerifyEmailTokenMail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
-    // \Illuminate\Support\Facades\Mail::to('alyredagomaa@gmail.com')->send(new VerifyEmailTokenMail());
-    // return 'email sent successfully';
-    return "App is live.";
+    # Seed Test User Using Path
+    \App\Models\User::create([
+        'name' => 'Test',
+        'email' => 'test@aly-h.com',
+        'verified_at' => now(),
+        'password' => bcrypt('Test123$$'),
+    ]);
+
+    return "User created successfully.";
+    // return "App is live.";
 });
