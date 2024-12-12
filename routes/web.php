1887a372c43388c9849392ba6205af58d7dd1da8
Ali Reda <alyredagomaa@gmail.com>
2024-12-12 15:59:01 +0200

    reset test path

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 Route::get('/test', function () {
-  // return "App is live.";
-  Schema::create('expenses', function (Blueprint $table) {
-    $table->id();
-    $table->string('name', 255)->index();
-    $table->decimal('price')->index();
-    $table->unsignedBigInteger('date_id')->index();
-    $table->unsignedInteger('category_id');
-    $table->timestamps();
-  });
-  Schema::create('dates', function (Blueprint $table) {
-    $table->id();
-    $table->unsignedBigInteger('user_id')->index();
-    $table->date('date')->index();
-    $table->decimal('expenses_sum')->nullable()->index();
-  });
-  return "Migrated tables successfully.";
+    return "App is live.";
 });
