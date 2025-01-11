b65dadb61063b3be8f57346b7cc9c1ff91482660
Ali Reda <alyredagomaa@gmail.com>
2025-01-11 19:55:40 +0200

    add mobile app version endpoint

--- routes/api.php
+++ routes/api.php
@@ -8,21 +8,28 @@ use App\Http\Controllers\ExpensesController;
 use App\Http\Controllers\Auth\PasswordController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
+});
+
+Route::get('/mobile-app-version', function (Request $request) {
+  return response()->json([
+    'status' => 'success',
+    'version' => '1.0.0'
+  ]);
 });
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::match(['put', 'patch'], '/profile-password', [PasswordController::class, 'update'])->name('profile.password.update');
-    Route::match(['put', 'patch'], '/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::match(['put', 'patch'], '/profile-password', [PasswordController::class, 'update'])->name('profile.password.update');
+  Route::match(['put', 'patch'], '/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::group(['middleware' => ['auth:sanctum' , 'verified']], function () {
-  Route::delete('expenses/delete-all', [ExpensesController::class , 'deleteAll'])->name('expenses.delete-all');
-  Route::post('expenses/seed', [ExpensesController::class , 'seed'])->name('expenses.seed');
-  Route::resource('expenses', ExpensesController::class)->only( 'create', 'store', 'update' , 'destroy');
-  
-  Route::resource('date', DateController::class)->only('index', 'show' );
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
+  Route::delete('expenses/delete-all', [ExpensesController::class, 'deleteAll'])->name('expenses.delete-all');
+  Route::post('expenses/seed', [ExpensesController::class, 'seed'])->name('expenses.seed');
+  Route::resource('expenses', ExpensesController::class)->only('create', 'store', 'update', 'destroy');
+
+  Route::resource('date', DateController::class)->only('index', 'show');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
